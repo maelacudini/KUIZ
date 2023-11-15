e1862dd50bf1d55d1d094da4c02dc503c9eb3742
maelacudini <maelacudini@gmail.com>
2023-11-15 19:12:43 +0100

    new commit

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,22 +1,22 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://acme.com',
+      url: 'https://https://kuiz-kappa.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
-    {
-      url: 'https://acme.com/about',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://acme.com/blog',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
+    // {
+    //   url: 'https://acme.com/about',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'monthly',
+    //   priority: 0.8,
+    // },
+    // {
+    //   url: 'https://acme.com/blog',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'weekly',
+    //   priority: 0.5,
+    // },
   ]
 }
