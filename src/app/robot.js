b843af099b879fec029d8bca344a7cd7862649cf
maelacudini <maelacudini@gmail.com>
2023-11-15 19:11:46 +0100

    new commit

---
+++ src/app/robot.js
@@ -0,0 +1,9 @@
+export default function robots() {
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+        },
+        sitemap: 'https://https://kuiz-kappa.vercel.app//sitemap.xml',
+    }
+}
