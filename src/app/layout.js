7cd8899e9d04325c86326569073503c6963812f7
maelacudini <maelacudini@gmail.com>
2023-11-15 14:05:27 +0100

    new commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Polystudio Website',
+  title: 'Kuiz | Art Museum',
   description: 'Made by Maela Cudini, 2023',
 }
 
